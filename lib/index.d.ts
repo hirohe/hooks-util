2475786ae313169a7b483ac7eb95da0c52386cbe
hirohe <hesreg@hotmail.com>
2020-03-05 14:07:14 +0800

    init

---
+++ lib/index.d.ts
@@ -0,0 +1,3 @@
+export declare type FetchAction<Data> = () => Promise<Data>;
+export declare function useFetch<T = undefined>(action: FetchAction<T>): [FetchAction<T>, T | undefined, boolean];
+export declare function useFetch<T>(action: FetchAction<T>, initialState: T): [FetchAction<T>, T, boolean];
