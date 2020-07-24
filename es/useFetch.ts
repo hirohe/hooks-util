0d239f57a14598badb56b0fd6c507f890d2f9964
hirohe <hesreg@hotmail.com>
2020-07-24 16:07:03 +0800

    update useFetch

--- es/useFetch.ts
+++ es/useFetch.ts
@@ -1,20 +1,20 @@
-import {useState} from "react";
+import { useCallback, useState } from 'react';
 
 export type FetchAction<Data> = () => Promise<Data>;
 
-export function useFetch<T = undefined>(action: FetchAction<T>): [FetchAction<T>, T | undefined, boolean];
-export function useFetch<T>(action: FetchAction<T>, initialState: T): [FetchAction<T>, T, boolean];
+export default function useFetch<T = undefined>(action: FetchAction<T>): [FetchAction<T>, T | undefined, boolean];
+export default function useFetch<T>(action: FetchAction<T>, initialState: T): [FetchAction<T>, T, boolean];
 
 export default function useFetch<T>(action: FetchAction<T>, initialState?: T): [FetchAction<T>, T | undefined, boolean] {
   const [data, setData] = useState<T | undefined>(initialState);
   const [loading, setLoading] = useState<boolean>(false);
 
-  function fetchData() {
+  const fetchData = useCallback(function() {
     if (action && typeof action === 'function') {
       setLoading(true);
       const result = action();
       if (result !== undefined && result instanceof Promise) {
-        return action().then((responseData: T) => {
+        return result.then((responseData: T) => {
           setData(responseData);
           return responseData;
         }).finally(() => {
@@ -26,7 +26,7 @@ export default function useFetch<T>(action: FetchAction<T>, initialState?: T): [
     } else {
       return Promise.reject('can not call action as function');
     }
-  }
+  }, [action]);
 
   return [fetchData, data, loading];
 }
