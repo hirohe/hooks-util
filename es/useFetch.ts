32e7380fb0e0b6fbf81dbf5e5a20b004d863a222
hirohe <hesreg@hotmail.com>
2020-11-27 17:28:25 +0800

    fix(ts): update type define of useFetch with initialState

--- es/useFetch.ts
+++ es/useFetch.ts
@@ -5,7 +5,7 @@ export type FetchAction<Data, Args extends any[] = any[]> = (...args: Args) => P
 type ThenArg<T> = T extends PromiseLike<infer U> ? U : T;
 
 export default function useFetch<T extends FetchAction<any>, D = ThenArg<ReturnType<T>>>(action: T): [T, D | undefined, boolean];
-export default function useFetch<T extends FetchAction<any>, D = ThenArg<ReturnType<T>>>(action: T, initialState: D): [T, D, boolean];
+export default function useFetch<T extends FetchAction<any>, D = ThenArg<ReturnType<T>>, InitState = D>(action: T, initialState: InitState): [T, D | InitState, boolean];
 
 export default function useFetch<T extends FetchAction<any>, D = ThenArg<ReturnType<T>>>(action: T, initialState?: D): [T, D | undefined, boolean] {
   const [data, setData] = useState<D | undefined>(initialState);
--- lib/useFetch.d.ts
+++ lib/useFetch.d.ts
@@ -1,4 +1,4 @@
 export declare type FetchAction<Data, Args extends any[] = any[]> = (...args: Args) => Promise<Data>;
 declare type ThenArg<T> = T extends PromiseLike<infer U> ? U : T;
 export default function useFetch<T extends FetchAction<any>, D = ThenArg<ReturnType<T>>>(action: T): [T, D | undefined, boolean];
-export default function useFetch<T extends FetchAction<any>, D = ThenArg<ReturnType<T>>>(action: T, initialState: D): [T, D, boolean];
+export default function useFetch<T extends FetchAction<any>, D = ThenArg<ReturnType<T>>, InitState = D>(action: T, initialState: InitState): [T, D | InitState, boolean];
